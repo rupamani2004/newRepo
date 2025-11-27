21d3b82187ad5a6a6a74bad6b75b2c49c8e44398
rupamani2004 <rupamani2004@gmail.com>
2025-11-27 23:03:00 +0530

    Code

---
+++ code1.js
@@ -0,0 +1,9 @@
+// Asynchronous example using setTimeout
+
+console.log("Message 1");
+
+setTimeout(function () {
+  console.log("Message 2 after 2 seconds");
+}, 2000); // 2000 ms = 2 seconds
+
+console.log("Message 3");
